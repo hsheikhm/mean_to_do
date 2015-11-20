1f08486b706f7b872b514e3857b775a0f9b946a7
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-20 18:21:20 +0000

    completed html

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var methodOverride = require('method-override');   // simulate DELETE and PUT (e
 
 mongoose.connect('mongodb://hamza:letmein@apollo.modulusmongo.net:27017/negUxo2z');   // connect to mongoDB database on modulus.io
 
-app.use(express.static('mean_to_do' + '/public'));  // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));  // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));   // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));   // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());  // parse application/json
